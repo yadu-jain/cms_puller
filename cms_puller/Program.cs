ccf742be27c5720cd9649e33890922d24cc191b8
yadu <yadu.jain@travelyaari.com>
2014-01-17 15:57:10 +0530

    First commit

---
+++ cms_puller/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+
+using System.Data;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Configuration;
+//using Newtonsoft.Json;
+//using System.IO;
+//using System.Data.SqlClient;
+
+
+namespace cms_puller
+{
+    class Program {
+        static void Main(string[] args) 
+        {
+            clsDB dbObj = new clsDB();
+            DataSet ds = dbObj.ExecuteSelect("GET_NEW_TRIPS_FOR_CRS_INFO_PULL", CommandType.StoredProcedure, 160);
+            
+            int cnt = 0;
+            string[] arrTrip_info = new string[ds.Tables[0].Rows.Count];
+            int[] arrTrip_ids = new int[ds.Tables[0].Rows.Count];            
+            int intTrip_id;
+            foreach (DataRow dataRow in ds.Tables[0].Rows)
+            {
+                try
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        string webUrl = ConfigurationSettings.AppSettings["WebserviceURL"].ToString();
+                        intTrip_id = Convert.ToInt32(dataRow["TRIP_ID"]);
+                        webUrl += "&KeyCode=''&intTripID=" + intTrip_id.ToString();
+                        webUrl += "&dtChartDate=" + ((DateTime)dataRow["CHART_DATE"]).ToString("yyyy-MMM-dd").ToString();
+                        byte[] obj = client.DownloadData(webUrl);
+                        string json = Encoding.ASCII.GetString(obj);
+                        arrTrip_info[cnt] = json;
+                        arrTrip_ids[cnt] = intTrip_id;
+                    }
+                }
+                catch (System.Exception es){}
+                cnt++;
+            }
+
+            int maxRow = 100;
+            DataTable dt = new DataTable();
+            //dt.TableName = "TRIP_INFO";
+            dt.Columns.Add("TRIP_ID", typeof(int));
+            dt.Columns.Add("ROUTE_INFO", typeof(string));
+            dt.Columns.Add("DEPARTURE_TIME", typeof(DateTime));
+            for (int i = 0; i < arrTrip_info.Length; i++)
+            {
+                if (arrTrip_info[i].Length > 0)
+                {
+                    string ctName = "";
+                    string deprTime = "";
+                    try
+                    {
+                        JObject tripInfo = JObject.Parse(arrTrip_info[i]);
+                        var cts = tripInfo["APITripsSummaryResult"].Children();
+                        foreach (var ct in cts)
+                        {
+                            ctName += ct["CityName"].ToString().Trim('"') + "-";
+                            if (ct["Position"].ToString().Trim('"') == "1")
+                                deprTime = ct["DepartureTime"].ToString().Trim('"');
+                        }
+                    }
+                    catch (System.Exception ex){}
+                    
+                    DataRow dr = dt.NewRow();
+                    dr["TRIP_ID"] = arrTrip_ids[i];
+                    dr["ROUTE_INFO"] = ctName.Remove(ctName.Length - 1);
+                    dr["DEPARTURE_TIME"] = Convert.ToDateTime(deprTime);
+                    dt.Rows.Add(dr);
+
+                    if (dt.Rows.Count == maxRow)
+                    {
+                        try
+                        {
+                            dbObj = null;
+                            dbObj = new clsDB();
+                            dbObj.AddParameter("TRIP_INFO", dt);
+                            dbObj.ExecuteDML("UPDATE_TRIP_INFO", CommandType.StoredProcedure, 180);
+                            dt.Rows.Clear();
+                        }
+                        catch (System.Exception ex){}
+                    }
+                }
+            }
+            
+            if (dt.Rows.Count > 0 && dt.Rows.Count < maxRow)
+            {
+                try
+                {
+                    dbObj = null;
+                    dbObj = new clsDB();
+                    dbObj.AddParameter("TRIP_INFO", dt);
+                    dbObj.ExecuteDML("UPDATE_TRIP_INFO", CommandType.StoredProcedure, 180);
+                    dt.Rows.Clear();
+                }
+                catch (System.Exception ex) { }
+            }
+        }
+    }
+
+}
---
+++ cms_puller/clsDB.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+//using System.Web.Security;
+//using System.Web.UI;
+//using System.Web.UI.HtmlControls;
+//using System.Web.UI.WebControls;
+//using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for clsDB
+/// </summary>
+public class clsDB
+{
+    SqlCommand cmd;
+    string strConnString;
+
+    public clsDB()
+    {
+        strConnString = ConfigurationSettings.AppSettings["ConnStr"].ToString();
+        StartNewCommand();
+    }
+
+    public void StartNewCommand()
+    {
+        cmd = new SqlCommand();
+    }
+
+    public void AddParameter(string strParamName, string strParamValue, int intParamSize)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.VarChar, intParamSize).Value = strParamValue;
+    }
+    public void AddParameter(string strParamName, int intParamValue)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.Int).Value = intParamValue;
+    }
+    public void AddParameter(string strParamName, bool blnParamValue)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.Bit).Value = blnParamValue;
+    }
+    public void AddParameter(string strParamName, DateTime dtParamValue)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.DateTime).Value = dtParamValue;
+    }
+    public void AddParameter(string strParamName, decimal dclParamValue)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.Decimal).Value = dclParamValue;
+    }
+    public void AddParameter(string strParamName, double dblParamValue)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.Decimal).Value = dblParamValue;
+    }
+    public void AddParameter(string strParamName, DataTable tablePara)
+    {
+        cmd.Parameters.Add("@" + strParamName, SqlDbType.Structured).Value = tablePara;
+    }
+    public DataTable FilterDataTable(DataTable dt, string strFilterQuery, string strSortBy)
+    {
+        DataTable dtTemp;
+        DataRow[] dr;
+        dr = dt.Select(strFilterQuery, strSortBy);
+        dtTemp = dt.Clone();
+
+        foreach (DataRow r in dr)
+        {
+            dtTemp.ImportRow(r);
+        }
+        return dtTemp;
+    }
+
+    public int ExecuteDML(string strSQL, CommandType cmdType, int intTimeout)
+    {
+        int status = 0;
+        SqlConnection conn = new SqlConnection(strConnString);
+
+        try
+        {
+            cmd.CommandText = strSQL;
+            cmd.CommandType = cmdType;
+            cmd.CommandTimeout = intTimeout;
+            conn.Open();
+            cmd.Connection = conn;
+            status = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+        }
+        catch (System.Exception ex)
+        {
+            status = -1;
+        }
+        finally
+        {
+            cmd.Cancel();
+            conn.Close();
+            conn.Dispose();
+        }
+        return status;
+    }
+
+    public DataSet ExecuteSelect(string strSQL, CommandType cmdType, int intTimeout)
+    {
+        DataSet ds = new DataSet();
+        SqlDataAdapter adp = new SqlDataAdapter();
+        SqlConnection conn = new SqlConnection(strConnString);
+        try
+        {
+            cmd.CommandText = strSQL;
+            cmd.CommandType = cmdType;
+            cmd.CommandTimeout = intTimeout;
+            adp.SelectCommand = cmd;
+            conn.Open();
+            cmd.Connection = conn;
+            adp.Fill(ds);
+            cmd.Parameters.Clear();
+        }
+        catch (System.Exception e)
+        {
+            ds = null;
+            //prateek- line below should be uncommented.
+            //throw e;
+        }
+        finally
+        {
+            adp.Dispose();
+            cmd.Cancel();
+            conn.Close();
+            conn.Dispose();
+        }
+        return ds;
+    }
+}
